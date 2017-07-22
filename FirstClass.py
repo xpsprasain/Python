690d2ea05526ecaa72d8980f949c2dc579883d8b
xpsprasain <xpsprasain@gmail.com>
2017-07-22 09:35:33 +0545

    Initial commit

---
+++ FirstClass.py
@@ -0,0 +1,20 @@
+# age=int(input("enter age\n"))
+#
+# if(age<20):
+#     if(age>11):
+#         print("teen")
+#     else:
+#         print("not team")
+# elif(age>20 and age<60):
+#     print("Adult")
+# else:
+#     print("old")
+
+t=(1,2,3)
+
+for i in t:
+    print(i)
+
+
+
+
